0e4af8855d5097bb7eb10547ce6261bd3455283d
Thomas Kristensen <thomas.kristensen@uswitch.com>
2014-12-19 16:09:32 +0000

    Switch to state machine in S3 rather than communicating with Redshift subsystem

--- src/uswitch/blueshift/s3.clj
+++ src/uswitch/blueshift/s3.clj
@@ -7,7 +7,9 @@
             [clojure.edn :as edn]
             [uswitch.blueshift.util :refer (close-channels clear-keys)]
             [schema.core :as s]
-            [metrics.counters :refer (counter inc! dec!)])
+            [metrics.counters :refer (counter inc! dec!)]
+            [metrics.timers :refer (timer time!)]
+            [uswitch.blueshift.redshift :as redshift])
   (:import [java.io PushbackReader InputStreamReader]
            [org.apache.http.conn ConnectionPoolTimeoutException]))
 
@@ -76,61 +78,107 @@
             (assoc-if-nil :strategy "merge")
             (update-in [:data-pattern] re-pattern))))))
 
-(defn- time-since
-  "Returns a function that returns the number of milliseconds since when
-  this function was invoked."
-  []
-  (let [now (System/currentTimeMillis)]
-    (fn []
-      (- (System/currentTimeMillis) now))))
+(defn- step-scan
+  [credentials bucket directory]
+  (try
+    (let [fs (files credentials bucket directory)]
+      (if-let [manifest (manifest credentials bucket fs)]
+        (do
+          (validate manifest)
+          (let [data-files  (filter (fn [{:keys [key]}]
+                                      (re-matches (:data-pattern manifest) key))
+                                    fs)]
+            (if (seq data-files)
+              (do
+                (info "Watcher triggering import" (:table manifest))
+                (debug "Triggering load:" load)
+                {:state :load, :table-manifest manifest, :files (map :key data-files)})
+              {:state :scan, :pause? true})))
+        {:state :scan, :pause? true}))
+    (catch clojure.lang.ExceptionInfo e
+      (error e "Error with manifest file")
+      {:state :scan, :pause? true})
+    (catch ConnectionPoolTimeoutException e
+      (warn e "Connection timed out. Will re-try.")
+      {:state :scan, :pause? true})
+    (catch Exception e
+      (error e "Failed reading content of" (str bucket "/" directory))
+      {:state :scan, :pause? true})))
 
-(defrecord KeyWatcher [credentials bucket directory redshift-load-ch poll-interval-seconds]
+(def importing-files (counter [(str *ns*) "importing-files" "files"]))
+(def import-timer (timer [(str *ns*) "importing-files" "time"]))
+
+(defn- step-load
+  [credentials bucket table-manifest files]
+  (let [redshift-manifest  (redshift/manifest bucket files)
+        {:keys [key url]}  (redshift/put-manifest credentials bucket redshift-manifest)]
+
+    (info "Importing" (count files) "data files to table" (:table table-manifest) "from manifest" url)
+    (debug "Importing Redshift Manifest" redshift-manifest)
+    (inc! importing-files (count files))
+    (try (time! import-timer
+                (redshift/load-table credentials url table-manifest))
+         (info "Successfully imported" (count files) "files")
+         (delete-object credentials bucket key)
+         (dec! importing-files (count files))
+         {:state :delete
+          :files files}
+         (catch java.sql.SQLException e
+           (error e "Error loading into" (:table table-manifest))
+           (error (:table table-manifest) "Redshift manifest content:" redshift-manifest)
+           (delete-object credentials bucket key)
+           (dec! importing-files (count files))
+           {:state :scan
+            :pause? true}))))
+
+(defn- step-delete
+  [credentials bucket files]
+  (do
+    (doseq [key files]
+      (info "Deleting" (str "s3://" bucket "/" key))
+      (try
+        (delete-object credentials bucket key)
+        (catch Exception e
+          (warn "Couldn't delete" key "  - ignoring"))))
+    {:state :scan, :pause? true}))
+
+(defn- progress
+  [{:keys [state] :as world}
+   {:keys [credentials bucket directory] :as configuration}]
+  (debug "world before stepping" world)
+  (case state
+    :scan   (step-scan   credentials bucket directory )
+    :load   (step-load   credentials bucket           (:table-manifest world) (:files world))
+    :delete (step-delete credentials bucket           (:files world))))
+
+(defrecord KeyWatcher [credentials bucket directory poll-interval-seconds]
   Lifecycle
   (start [this]
     (info "Starting KeyWatcher for" (str bucket "/" directory) "polling every" poll-interval-seconds "seconds")
-    (let [control-ch (chan)]
+    (let [control-ch    (chan)
+          configuration {:credentials credentials :bucket bucket :directory directory}]
       (thread
-       (loop []
-         (let [elapsed-time (time-since)]
-           (try
-             (let [fs (files credentials bucket directory)]
-               (when-let [manifest (manifest credentials bucket fs)]
-                 (validate manifest)
-                 (let [data-files  (filter (fn [{:keys [key]}]
-                                             (re-matches (:data-pattern manifest) key))
-                                           fs)
-                       complete-ch (chan)
-                       load        {:table-manifest manifest
-                                    :files          (map :key data-files)
-                                    :complete-ch    complete-ch}]
-                   (when (seq data-files)
-                     (info "Watcher triggering import" (:table manifest))
-                     (debug "Triggering load:" load)
-                     (>!! redshift-load-ch load)
-                     (debug "Waiting for completion")
-                     (<!! complete-ch)))))
-             (catch clojure.lang.ExceptionInfo e
-               (error e "Error with manifest file"))
-             (catch ConnectionPoolTimeoutException e
-               (warn e "Connection timed out. Will re-try in" poll-interval-seconds "seconds"))
-             (catch Exception e
-               (error e "Failed reading content of" (str bucket "/" directory))))
-           (let [pause-millis (max 0 (- (* poll-interval-seconds 1000) (elapsed-time)))
-                 [_ c] (alts!! [control-ch (timeout pause-millis)])]
-             (when (not= c control-ch)
-               (recur))))))
+       (loop [timer (timeout (* poll-interval-seconds 1000))
+              world {:state :scan}]
+         (let [next-world (progress world configuration)]
+           (debug "next-world to step" next-world)
+           (if (:pause? next-world)
+             (let [[_ c] (alts!! [control-ch timer])]
+               (when (not= c control-ch)
+                 (recur (timeout (* poll-interval-seconds 1000)) next-world)))
+             (recur timer next-world)))))
       (assoc this :watcher-control-ch control-ch)))
   (stop [this]
     (info "Stopping KeyWatcher for" (str bucket "/" directory))
     (close-channels this :watcher-control-ch)))
 
 
-(defn spawn-key-watcher! [credentials bucket directory redshift-load-ch poll-interval-seconds]
-  (start (KeyWatcher. credentials bucket directory redshift-load-ch poll-interval-seconds)))
+(defn spawn-key-watcher! [credentials bucket directory poll-interval-seconds]
+  (start (KeyWatcher. credentials bucket directory poll-interval-seconds)))
 
 (def directories-watched (counter [(str *ns*) "directories-watched" "directories"]))
 
-(defrecord KeyWatcherSpawner [bucket-watcher redshift-load-ch poll-interval-seconds]
+(defrecord KeyWatcherSpawner [bucket-watcher poll-interval-seconds]
   Lifecycle
   (start [this]
     (info "Starting KeyWatcherSpawner")
@@ -139,7 +187,7 @@
       (go-loop [dirs (<! new-directories-ch)]
         (when dirs
           (doseq [dir dirs]
-            (swap! watchers conj (spawn-key-watcher! credentials bucket dir redshift-load-ch poll-interval-seconds))
+            (swap! watchers conj (spawn-key-watcher! credentials bucket dir poll-interval-seconds))
             (inc! directories-watched))
           (recur (<! new-directories-ch))))
       (assoc this :watchers watchers)))
@@ -193,27 +241,6 @@
                        :key-pattern (or (re-pattern (-> config :s3 :key-pattern))
                                         #".*")}))
 
-(defrecord Cleaner [credentials bucket cleaner-ch]
-  Lifecycle
-  (start [this]
-    (info "Starting Cleaner")
-    (thread
-     (loop []
-       (when-let [m (<!! cleaner-ch)]
-         (doseq [key (:files m)]
-           (info "Deleting" (str "s3://" bucket "/" key))
-           (delete-object credentials bucket key))
-         (recur))))
-    this)
-  (stop [this]
-    (info "Stopping Cleaner")
-    this))
-
-(defn cleaner [config]
-  (map->Cleaner {:credentials (-> config :s3 :credentials)
-                 :bucket      (-> config :s3 :bucket)}))
-
-
 (defrecord PrintSink [prefix chan-k component]
   Lifecycle
   (start [this]
@@ -230,10 +257,7 @@
   [prefix chan-k]
   (map->PrintSink {:prefix prefix :chan-k chan-k}))
 
-
 (defn s3-system [config]
-  (system-map :cleaner (using (cleaner config)
-                              [:cleaner-ch])
-              :bucket-watcher (bucket-watcher config)
+  (system-map :bucket-watcher (bucket-watcher config)
               :key-watcher-spawner (using (key-watcher-spawner config)
-                                          [:bucket-watcher :redshift-load-ch])))
+                                          [:bucket-watcher])))
--- src/uswitch/blueshift/system.clj
+++ src/uswitch/blueshift/system.clj
@@ -1,24 +1,10 @@
 (ns uswitch.blueshift.system
   (:require [uswitch.blueshift.s3 :refer (s3-system)]
-            [uswitch.blueshift.redshift :refer (redshift-system)]
             [uswitch.blueshift.telemetry :refer (telemetry-system)]
             [com.stuartsierra.component :refer (system-map using Lifecycle start)]
             [clojure.core.async :refer (chan close!)])
   (:import [clojure.core.async.impl.channels ManyToManyChannel]))
 
-
-;; TODO
-;; Don't depend on underlying channel type
-(extend-type ManyToManyChannel
-  Lifecycle
-  (stop [this] (when this (close! this)))
-  (start [this] this))
-
 (defn build-system [config]
-  (system-map :s3-system (using (s3-system config)
-                                [:redshift-load-ch :cleaner-ch])
-              :redshift-load-ch (chan 100)
-              :redshift-system (using (redshift-system config)
-                                      [:redshift-load-ch :cleaner-ch])
-              :cleaner-ch (chan 100)
+  (system-map :s3-system (s3-system config)
               :telemetry-system (telemetry-system config)))
