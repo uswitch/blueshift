768b5d6d7832225948d8ef5c962ef80c3014cece
Waldemar <waldemar.schwan@uswitch.com>
2019-11-04 13:42:16 +0000

    default exception handler

--- src/uswitch/blueshift/main.clj
+++ src/uswitch/blueshift/main.clj
@@ -1,9 +1,20 @@
 (ns uswitch.blueshift.main
-  (:require [clojure.tools.logging :refer (info)]
+  (:require [clojure.tools.logging :refer (info error)]
             [clojure.tools.cli :refer (parse-opts)]
             [uswitch.blueshift.system :refer (build-system)]
             [com.stuartsierra.component :refer (start stop)])
   (:gen-class))
+
+(defn register-default-exception-handler!
+  [system]
+  (Thread/setDefaultUncaughtExceptionHandler
+   (reify Thread$UncaughtExceptionHandler
+     (uncaughtException [_ thread e]
+       (error "Unhanled exception in thread " (str thread))
+       (error e)
+       (error "Caucht unhandled exception! Stopping system and terminating!")
+       (stop system)
+       (System/exit 2)))))
 
 (def cli-options
   [["-c" "--config CONFIG" "Path to EDN configuration file"
@@ -23,5 +34,6 @@
     (let [{:keys [config]} options]
       (info "Starting Blueshift with configuration" config)
       (let [system (build-system (read-string (slurp config)))]
+        (register-default-exception-handler! system)
         (start system)
         (wait!)))))
